d394fbb59724db503b515f3abc94233ad132329e
brigittewarner <brigitte.warner@gmail.com>
2018-09-25 12:14:25 -0400

    Initial Commit

---
+++ NightVale/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NightVale
+//
+//  Created by Brigitte Warner on 9/25/18.
+//  Copyright © 2018 Brigitte Warner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NightVale/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NightVale
+//
+//  Created by Brigitte Warner on 9/25/18.
+//  Copyright © 2018 Brigitte Warner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
